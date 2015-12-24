a7ece440e210dc30d1f345315991c7a51f7303ec
manhnhhedspi <manhnhhedspi@gmail.com>
2015-12-25 00:29:43 +0700

    Add file laptop.cc and file robot.cc

---
+++ laptop.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	system("gnome-terminal -x sh -c 'cd /home/GR/catkin_ws/wandrian ; roslaunch gscam gscam_nodelet.launch'");
+	system("gnome-terminal -x sh -c 'cd /home/GR/catkin_ws/wandrian ; rosrun mjpeg_server mjpeg_server'");
+	cout << "Done\n";
+	return 0;
+}
